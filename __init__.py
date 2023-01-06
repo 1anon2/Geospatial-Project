a50adb0359a4138f988e09fc5438192b2551050f
flavio <flavio_rossi1@outlook.com>
2023-01-06 17:09:38 +0100

    Update PathCompare.py and __init__.py

--- __init__.py
+++ __init__.py
@@ -2,9 +2,9 @@ from Core.PathCompare import PathCompare
 
 
 def main():
-    path = PathCompare(name='MilanoData.csv')
+    path = PathCompare(name='MilanoData.csv', num_of_process=8)
     path.load_graph(graph_area='Milan, Italy', mode='drive')
-    path.run()
+    path.run(city='Milano')
 
 
 if __name__ == "__main__":
