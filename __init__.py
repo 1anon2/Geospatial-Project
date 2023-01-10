8aa01ff511a87cb92ebd0eaba8120615653fe9bc
flavio <flavio_rossi1@outlook.com>
2023-01-10 13:41:04 +0100

    Update Metrics_2023-01-06-00-17-52.csv, Metrics_2023-01-06-14-08-08.csv, and 3 more files...

--- __init__.py
+++ __init__.py
@@ -1,5 +1,6 @@
 from Core.PathCompare import PathCompare
 
+# WARNING: processing MilanoData takes about 3.5 hours using multiprocessing with 8cores
 
 def main():
     path = PathCompare(name='MilanoData.csv', num_of_process=8)
